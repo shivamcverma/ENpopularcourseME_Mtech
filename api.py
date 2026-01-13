99d5933f99dba143fff7b87707be37dbdc4cc0db
Shivam Verma <sv726544@gmail.com>
2026-01-13 17:10:19 +0530

    api

--- api.py
+++ api.py
@@ -46,7 +46,7 @@ def root():
 
 
 # 🔹 Full data
-@app.get("/btech_popular_course_BE_Btech")
+@app.get("/mtech_popular_course_ME_Mtech")
 def get_all_data():
     return {
         "data": load_data()
@@ -54,7 +54,7 @@ def get_all_data():
 
 
 # 🔹 Access ANY section by name
-@app.get("//btech_popular_course_BE_Btech/{section_name}")
+@app.get("//mtech_popular_course_ME_Mtech/{section_name}")
 def get_section_by_name(section_name: str):
     data = load_data()
     result = find_section(data, section_name)
